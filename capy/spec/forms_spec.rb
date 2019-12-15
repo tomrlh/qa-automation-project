872974c980f38d4c549c59f92969b9ac68e6cb96
Thiago Medeiros <si-thiago@hotmail.com>
2019-12-15 03:42:44 -0300

    Testing element content

--- capy/spec/forms_spec.rb
+++ capy/spec/forms_spec.rb
@@ -9,5 +9,12 @@ describe 'Forms' do
         click_button 'Login'
 
         expect(find('#flash').visible?).to be true
+
+        # include is equivalent to 'contains'
+        # 'contains' check in string
+        expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!'
+
+        # 'have_content' checks directly in element
+        expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
     end
 end
