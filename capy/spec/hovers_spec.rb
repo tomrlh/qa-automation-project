0db1af97aa5e3efb38c99dace9180f245abcc35e
Thiago Medeiros <si-thiago@hotmail.com>
2019-12-23 22:31:11 -0300

    Hovers testing

---
+++ capy/spec/hovers_spec.rb
@@ -0,0 +1,32 @@
+describe 'Mouse Hover', :hovers do
+
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/hovers'
+    end
+
+    it 'when hovers Blade' do
+        card = find('img[alt=Blade]')
+        card.hover
+
+        expect(page).to have_content 'Nome: Blade'
+    end
+
+    it 'when hovers Black-Panther' do
+        card = find('img[alt="Pantera Negra"]')
+        card.hover
+
+        expect(page).to have_content 'Nome: Pantera Negra'
+    end
+
+    it 'when hovers Spider-Man' do
+        card = find('img[alt="Homem Aranha"]')
+        card.hover
+
+        expect(page).to have_content 'Nome: Homem Aranha'
+    end
+
+    after(:each) do
+        sleep 0.5
+    end
+
+end
