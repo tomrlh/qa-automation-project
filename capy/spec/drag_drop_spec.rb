d25d04b2fd8dfbf1f33c8a69b6b25b0cbe57f3bc
Thiago Medeiros <si-thiago@hotmail.com>
2019-12-23 22:38:22 -0300

    Drag-n-drop automated test implemented

---
+++ capy/spec/drag_drop_spec.rb
@@ -0,0 +1,18 @@
+describe 'Drag and Drop', :drop do
+
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/drag_and_drop'
+    end
+
+    it 'Spider-Man is from Stark team' do
+        stark = find('.team-stark .column')
+        cap = find('.team-cap .column')
+
+        spiderman = find('img[alt*=Aranha]') # * is regex
+        spiderman.drag_to stark
+
+        expect(stark).to have_css 'img[alt$=Aranha]'
+        expect(cap).not_to have_css 'img[alt$=Aranha]'
+    end
+
+end
