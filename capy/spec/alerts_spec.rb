c81c48886a6b0caa0650e14e3ae7291b44a57c08
Thiago Medeiros <si-thiago@hotmail.com>
2019-12-27 00:07:14 -0300

    Interacting with JS alert windows (alert, prompt and confirmation

---
+++ capy/spec/alerts_spec.rb
@@ -0,0 +1,56 @@
+describe 'JS alerts', :alerts do
+    before(:each) do
+        visit 'http://training-wheels-protocol.herokuapp.com/javascript_alerts' 
+    end
+
+    it 'alert' do
+        click_button 'Alerta'
+
+        msg = page.driver.browser.switch_to.alert.text
+
+        expect(msg).to eql 'Isto é uma mensagem de alerta'
+    end
+
+    it 'confirmation confirm' do
+        click_button 'Confirma'
+
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'E ai confirma?'
+
+        page.driver.browser.switch_to.alert.accept
+        expect(page).to have_content 'Mensagem confirmada'
+    end
+
+    it 'confirmation cancel' do
+        click_button 'Confirma'
+
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'E ai confirma?'
+
+        page.driver.browser.switch_to.alert.dismiss
+        expect(page).to have_content 'Mensagem não confirmada'
+    end
+
+    it 'accept prompt', :accept_prompt do
+        name = 'tomrlh'
+
+        accept_prompt(with: name) do
+            click_button 'Prompt'
+        end
+
+        expect(page).to have_content 'Olá, ' + name
+        sleep 2
+    end
+
+    # challenge
+
+    it 'accept prompt', :dismiss_prompt do
+
+        dismiss_prompt() do
+            click_button 'Prompt'
+        end
+        
+        expect(page).to have_content 'Olá, null'
+        sleep 2
+    end    
+end
