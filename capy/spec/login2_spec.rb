6337974a28312e8ba5657bb4f0f14044031930dc
Thiago Medeiros <si-thiago@hotmail.com>
2019-12-27 00:24:06 -0300

    Filling randomic field forms

---
+++ capy/spec/login2_spec.rb
@@ -0,0 +1,54 @@
+describe 'Login 2', :login2 do
+    # common function
+    def login(
+        url, user, password, user_field, password_field,
+        submit_button, message_field, expected_message
+    )
+        visit url
+
+        find('#userId').set user
+        find('#passId').set password
+
+        login_form = find('#login')
+
+        case login_form.text
+        when /Dia/
+            find('#day').set '29'
+        when /Mês/
+            find('#month').set '05'
+        when /Ano/
+            find('#year').set '1970'
+        end
+
+        sleep 2
+
+        click_button submit_button
+        expect(find(message_field)).to have_content expected_message
+    end
+
+
+
+    # global variables
+    $url = 'http://training-wheels-protocol.herokuapp.com/login2'
+    $user = 'stark'
+    $password = 'jarvis!'
+    $wrong_user = 'wrong_user'
+    $wrong_password = 'wrong_password'
+
+    $user_field = 'userId'
+    $password_field = 'password'
+    $submit_button = 'Login'
+    $message_field = '#flash'
+
+
+
+    it 'successful login' do
+        # @ for local variables
+        @expected_message = 'Olá, Tony Stark. Você acessou a área logada!'
+
+        login(
+                $url, $user, $password, $user_field, $password_field,
+                $submit_button, $message_field, @expected_message
+        )
+    end
+end
