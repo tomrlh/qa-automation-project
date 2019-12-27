2ff282168d67dd360946970615c93b3d81675224
Thiago Medeiros <si-thiago@hotmail.com>
2019-12-27 00:13:25 -0300

    Opening a new window

---
+++ capy/spec/windows_spec.rb
@@ -0,0 +1,19 @@
+describe 'alternating windows', :windows do
+    before(:each) do
+        visit 'http://training-wheels-protocol.herokuapp.com/windows'
+    end
+
+    it 'new window' do
+
+        new_window = window_opened_by {click_link 'Clique aqui'}
+
+        within_window -> {page.title == 'Nova Janela'} do
+            expect(page).to have_content 'Aqui temos uma nova janela \o/'
+        end
+
+        new_window.close
+
+        sleep 3
+
+    end
+end
