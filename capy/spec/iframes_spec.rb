cb2454f7cd021b0658b5eac11a1ae0c7c69cb73c
Thiago Medeiros <si-thiago@hotmail.com>
2019-12-28 20:07:28 -0300

    Accessing IFrames with Capybara

---
+++ capy/spec/iframes_spec.rb
@@ -0,0 +1,38 @@
+describe 'iframes' do
+    describe 'good', :nice_iframe do
+
+        before(:each) do
+            visit 'http://training-wheels-protocol.herokuapp.com/nice_iframe'
+        end
+
+        it 'add to cart' do
+            within_frame('burgerId') do
+                product = find('.menu-item-info-box', text: 'REFRIGERANTE')
+                product.find('a').click
+
+                expect(find('#cart')).to have_content 'R$ 4,50'
+                sleep 3
+            end
+        end
+    end
+
+
+
+    describe 'bad', :bad_iframe do
+
+        before(:each) do
+            visit 'http://training-wheels-protocol.herokuapp.com/bad_iframe'
+        end
+
+        it 'cart is empty' do
+
+            scriptToAddIdToIframe = '$(".box-iframe").attr("id", "tempId")'
+
+            page.execute_script(scriptToAddIdToIframe)
+
+            within_frame('tempId') do
+                expect(find('#cart')).to have_content 'Seu carrinho está vazio!'
+            end
+        end
+    end
+end
